cf6de167cdc452c9cc15cf48c7845db8aca40ddb
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-12-13 06:33:43 +0800

    renames

--- OBS_Status/WebSocket/Client.cs
+++ OBS_Status/WebSocket/Client.cs
@@ -15,34 +15,35 @@ namespace OBS_Status.WebSocket
 
 		public async Task ConnectAndSendMessageAsync()
 		{
-			System.Diagnostics.Debug.WriteLine("hello sending ws");
-
 			try
 			{
+				// create the websocket
 				ws = new MessageWebSocket();
 				ws.Control.MessageType = SocketMessageType.Utf8;
 
-				ws.MessageReceived += Ws_MessageReceived;
+				// register message handler
+				ws.MessageReceived += OnMessageReceive;
 
+				// connect to the obs server
 				string endpoint = $"ws://{ServerAddress}:{ServerPort}";
 				Debug.WriteLine($"Connecting to OBS at {endpoint}...");
-
 				await ws.ConnectAsync(new Uri(endpoint));
 
+				// connection establishd
 				Debug.WriteLine("Connected!");
-
 				writer = new DataWriter(ws.OutputStream);
 			}
 			catch (Exception ex)
 			{
-				System.Diagnostics.Debug.WriteLine("[WebSocket] " + ex.Message);
+				Debug.WriteLine("[WebSocket] " + ex.Message);
 			}
 		}
 
-		private void Ws_MessageReceived(MessageWebSocket sender, MessageWebSocketMessageReceivedEventArgs args)
+		private void OnMessageReceive(MessageWebSocket sender, MessageWebSocketMessageReceivedEventArgs args)
 		{
 			using (var reader = args.GetDataReader())
 			{
+				// read the message
 				string msg = reader.ReadString(reader.UnconsumedBufferLength);
 				Debug.WriteLine("OBS → " + msg);
 
