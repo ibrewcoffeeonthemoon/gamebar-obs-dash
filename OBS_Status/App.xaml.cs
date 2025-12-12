20f6d0fbd333104f679721e2ecddc33fd8012fc9
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-12-13 05:40:45 +0800

    better namings

--- OBS_Status/App.xaml.cs
+++ OBS_Status/App.xaml.cs
@@ -16,7 +16,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Microsoft.Gaming.XboxGameBar;
 
-namespace GamebarOBSDash
+namespace OBS_Status
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
