5d3c3f00e3d01d1fb8ad380ca6a96fe6c6e04897
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-12-14 19:04:12 +0800

    move state var

--- OBS_Status/WebSocket/Client.State.cs
+++ OBS_Status/WebSocket/Client.State.cs
@@ -1,29 +1,6 @@
-﻿using Windows.UI;
-using Windows.UI.Core;
-using Windows.UI.Xaml.Media;
-
-namespace OBS_Status.WebSocket
+﻿namespace OBS_Status.WebSocket
 {
     public partial class Client
     {
-		private bool _isRecording;
-		public bool IsRecording
-		{
-			get => _isRecording;
-			set
-			{
-				// update internal state
-				_isRecording = value;
-
-				// Only update recording state color if already connected
-				if (IsConnected)
-				{
-					Page?.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-					{
-						Page.StatusBorder.Background = new SolidColorBrush(value ? Colors.Red : Colors.DarkGreen);
-					});
-				}
-			}
-		}
 	}
 }
