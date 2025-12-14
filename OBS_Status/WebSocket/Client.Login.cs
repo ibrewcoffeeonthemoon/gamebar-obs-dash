056fcec9821b7fbfdeca91f2bddaddd9a1b95391
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-12-14 19:00:23 +0800

    move state var

--- OBS_Status/WebSocket/Client.Login.cs
+++ OBS_Status/WebSocket/Client.Login.cs
@@ -1,16 +1,39 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
-using System.IO;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Windows.Storage.Streams;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Media;
 
 namespace OBS_Status.WebSocket
 {
     public partial class Client
 	{
+		// State
+		private bool _isConnected;
+		public bool IsConnected
+		{
+			get => _isConnected;
+			set
+			{
+				// update internal state
+				_isConnected = value;
+
+				// Manual, safe UI update using stored page reference 
+				Page?.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+				{
+					Page.StatusBorder.Background = new SolidColorBrush(value ? Colors.DarkGreen : Colors.DarkSlateBlue);
+					// Add more if you have text or dot:
+					// Page.statusTextBlock.Text = value ? "Connected" : "Disconnected";
+					// Page.statusDot.Fill = new SolidColorBrush(value ? Colors.LightGreen : Colors.DarkSlateBlue);
+				});
+			}
+		}
+
 		private string password = File.ReadAllText(Path.Combine(System.AppContext.BaseDirectory, ".password"));
 
 		private async void Identify(JToken helloData)
--- OBS_Status/WebSocket/Client.State.cs
+++ OBS_Status/WebSocket/Client.State.cs
@@ -6,27 +6,6 @@ namespace OBS_Status.WebSocket
 {
     public partial class Client
     {
-		// State
-		private bool _isConnected;
-		public bool IsConnected
-		{
-			get => _isConnected;
-			set
-			{
-				// update internal state
-				_isConnected = value;
-
-				// Manual, safe UI update using stored page reference 
-				Page?.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-				{
-					Page.StatusBorder.Background = new SolidColorBrush(value ? Colors.DarkGreen : Colors.DarkSlateBlue);
-					// Add more if you have text or dot:
-					// Page.statusTextBlock.Text = value ? "Connected" : "Disconnected";
-					// Page.statusDot.Fill = new SolidColorBrush(value ? Colors.LightGreen : Colors.DarkSlateBlue);
-				});
-			}
-		}
-
 		private bool _isRecording;
 		public bool IsRecording
 		{
