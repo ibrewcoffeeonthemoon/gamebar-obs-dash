cd790c3bc1a8134829c875bac4b44ab73ab64fcc
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-12-17 06:54:06 +0800

    persists settings value in LocalSettings

--- OBS_Status/WidgetSettingsPage.xaml.cs
+++ OBS_Status/WidgetSettingsPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Windows.Storage;
 using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -13,11 +14,25 @@ namespace OBS_Status
         public WidgetSettingsPage()
         {
             this.InitializeComponent();
+			LoadSettings();
         }
 
-        private void OnSave(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		void LoadSettings()
+		{
+			var settings = ApplicationData.Current.LocalSettings;
+
+			AddressTextBox.Text = settings.Values["ObsAddress"] as string ?? "127.0.0.1";
+			PortTextBox.Text = settings.Values["ObsPort"] as string ?? "4455";
+			PasswordBox.Password = settings.Values["ObsPassword"] as string ?? "";
+		}
+
+		private void OnSave(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-            Debug.WriteLine("Save button clicked in WidgetSettingsPage.");
+			var settings = ApplicationData.Current.LocalSettings;
+
+			settings.Values["ObsAddress"] = AddressTextBox.Text?.Trim();
+			settings.Values["ObsPort"] = PortTextBox.Text?.Trim();
+			settings.Values["ObsPassword"] = PasswordBox.Password;
 		}
 	}
 }
